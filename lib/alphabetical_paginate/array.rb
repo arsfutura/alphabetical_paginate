2ddcb3cda698f62be79613492805161fabe93209
ViskoPl <visko@arsfutura.co>
2019-01-23 14:35:29 +0100

    Add sort parameter, so that we can controll if items should be sorted or not.

--- lib/alphabetical_paginate/array.rb
+++ lib/alphabetical_paginate/array.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 # Warning! this is not the original alpha_paginate. Made changes to charasters support (croatian letters). Original can be found on:  https://github.com/lingz/alphabetical_paginate
 class Array
-  def alpha_paginate current_field, params = {enumerate:false, default_field: "a", 
+  def alpha_paginate current_field, params = {enumerate:false, default_field: "a",
                                               paginate_all: false, numbers: true, include_all: true,
                                               others: true, pagination_class: "pagination-centered",
                                               js: true, support_language: :en, bootstrap3: false,
-                                              slugged_link: false, slug_field: "slug", all_as_link: true}
+                                              slugged_link: false, slug_field: "slug", all_as_link: true, sort: true}
     params[:paginate_all] ||= false
     params[:support_language] ||= :en
     params[:language] = AlphabeticalPaginate::Language.new(params[:support_language])
@@ -19,6 +19,7 @@ class Array
     params[:slugged_link] = params[:slugged_link] && defined?(Babosa)
     params[:slug_field] ||= "slug"
     params[:all_as_link] = true if !params.has_key? :all_as_link
+    params[:sort] = true if !params.has_key? :sort
 
     output = []
     availableLetters = {}
@@ -55,7 +56,7 @@ class Array
     params[:currentField] = current_field.mb_chars.capitalize.to_s
     params[:availableLetters] = params[:availableLetters][0..1] + params[:availableLetters][2..params[:availableLetters].length].sort.to_a
 
-    output.sort! {|x, y| block_given? ? (yield(x).to_s <=> yield(y).to_s) : (x.id.to_s <=> y.id.to_s) }
+    output.sort! {|x, y| block_given? ? (yield(x).to_s <=> yield(y).to_s) : (x.id.to_s <=> y.id.to_s) } if params[:sort]
     return output, params
   end
 end
--- lib/alphabetical_paginate/controller_helper.rb
+++ lib/alphabetical_paginate/controller_helper.rb
@@ -12,7 +12,7 @@ module AlphabeticalPaginate
                                                 db_field: "id", include_all: true,
                                                 js: true, support_language: :en,
                                                 bootstrap3: false, slugged_link: false,
-                                                slug_field: "slug", all_as_link: true}
+                                                slug_field: "slug", all_as_link: true, sort: true}
       params[:default_field] ||= "a"
       params[:paginate_all] ||= false
       params[:support_language] ||= :en
@@ -29,6 +29,7 @@ module AlphabeticalPaginate
       params[:slugged_link] = params[:slugged_link] && defined?(Babosa)
       params[:slug_field] ||= "slug"
       params[:all_as_link] = true if !params.has_key? :all_as_link
+      params[:sort] = true if !params.has_key? :sort
 
 
       output = []
@@ -86,26 +87,26 @@ module AlphabeticalPaginate
           field_letter = field_val[0].mb_chars.downcase.to_s
 
           if  ["A","B","C","Č","Ć","D","Dž","E","F","G","H","I","J","K","L","Lj","M","N","Nj","O","P","Q","R","S","Š","T","U","V","W","X","Y","Z","Ž"].include?(field_letter.upcase)
-              availableLetters[field_letter] = true if !availableLetters.has_key? field_letter
-              field = params[:slugged_link] ? slug : field_letter
+            availableLetters[field_letter] = true if !availableLetters.has_key? field_letter
+            field = params[:slugged_link] ? slug : field_letter
 
-              output << x if all || (current_field == field)
-            elsif /[0-9]/.match(field_letter)
-              if params[:enumerate]
-                availableLetters[field_letter] = true if !availableLetters.has_key? field_letter
-                output << x if all || (current_field =~ /[0-9]/ && field_letter == current_field)
-              else
-                availableLetters['0-9'] = true if !availableLetters.has_key? 'numbers'
-                output << x if all || current_field == "0-9"
-              end
+            output << x if all || (current_field == field)
+          elsif /[0-9]/.match(field_letter)
+            if params[:enumerate]
+              availableLetters[field_letter] = true if !availableLetters.has_key? field_letter
+              output << x if all || (current_field =~ /[0-9]/ && field_letter == current_field)
             else
-              availableLetters['*'] = true if !availableLetters.has_key? 'other'
-              output << x if all || current_field == "*"
+              availableLetters['0-9'] = true if !availableLetters.has_key? 'numbers'
+              output << x if all || current_field == "0-9"
+            end
+          else
+            availableLetters['*'] = true if !availableLetters.has_key? 'other'
+            output << x if all || current_field == "*"
           end
         end
 
         params[:availableLetters] = availableLetters.collect{ |k,v| k.mb_chars.capitalize.to_s }
-        output.sort! {|x, y| block_given? ? (yield(x).to_s <=> yield(y).to_s) : (x.id.to_s <=> y.id.to_s) }
+        output.sort! {|x, y| block_given? ? (yield(x).to_s <=> yield(y).to_s) : (x.id.to_s <=> y.id.to_s) } if params[:sort]
       end
       params[:currentField] = current_field.mb_chars.capitalize.to_s
 
@@ -131,7 +132,7 @@ module AlphabeticalPaginate
         else
           nil
         end
-      # repass again to filter duplicates *
+        # repass again to filter duplicates *
       end.uniq
     end
 
